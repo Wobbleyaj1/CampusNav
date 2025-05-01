e1fa64f7aeb54ae46a455515e3304f3a416a3cad
Christopher Brown <gb12cb1@gmail.com>
2025-05-01 14:38:28 -0400

    tests can be run from 1 file

---
+++ tests/all_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from graph_tester import graph_tests
+from queue_tester import queue_tests
+from stack_tester import stack_tests
+
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTest(loader.loadTestsFromTestCase(graph_tests))
+    suite.addTest(loader.loadTestsFromTestCase(queue_tests))
+    suite.addTest(loader.loadTestsFromTestCase(stack_tests))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
